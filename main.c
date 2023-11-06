efe5616f40efd972ac7ab8021df652de59a7b601
bitfl0wer <florian@pro-weber.com>
2023-11-06 14:41:12 +0100

    replace int-input with char[]

--- main.c
+++ main.c
@@ -5,26 +5,15 @@
 
 uint64_t *genbintable(char *binary);
 
-char *to_string(uint64_t num) {
-    uint64_t len = snprintf(NULL, 0, "%llu", num);
-    char *str = malloc(len + 1);
-    if (str == NULL) {
-        return 0;
-    }
-    snprintf(str, len + 1, "%llu", num);
-    return str;
-}
-
-uint64_t bin_to_dec(uint64_t bin) {
-    char *str = to_string(bin);
-    size_t length_str = strlen(str);
-    uint64_t *bintable = genbintable(str);
-    uint64_t result = 0;
-    for (size_t iter = 0; iter < length_str; iter++) {
-        if (str[iter] - '0' != 0 && str[iter] - '0' != 1) {
-            return -1;
+uint64_t bin_to_dec(char *bin) {
+    size_t length_str = strlen(bin);
+    uint64_t *bintable = genbintable(bin);
+    uint64_t result = 0ULL;
+    for (size_t iter = 0ULL; iter < length_str; iter++) {
+        if (bin[iter] - '0' != 0ULL && bin[iter] - '0' != 1ULL) {
+            return -1ULL;
         }
-        result += (str[iter] - '0') * (bintable[iter]);
+        result += (bin[iter] - '0') * (bintable[iter]);
     }
     return result;
 }
@@ -42,6 +31,6 @@ uint64_t *genbintable(char *binary) {
 }
 
 int main() {
-    printf("%llu\n", bin_to_dec(101010110101010010100110100101));
+    printf("%llu\n", bin_to_dec("1111111111111111111111111111111"));
     return 1;
 };
