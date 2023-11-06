7fcc2a08b39b6b3a58c0e60189bd6984600149be
bitfl0wer <florian@pro-weber.com>
2023-11-06 01:20:34 +0100

    impl working (see footnotes)* BIN->DEC converter If you change uint64_t to int throughout the project, the program works. however, i am still trying to figure out why specifiying a longer integer (such as 64bit) doesn't compile

--- main.c
+++ main.c
@@ -1,56 +1,47 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char *to_string(int num) {
-    int len = snprintf(NULL, 0, "%d", num);
+uint64_t *genbintable(char *binary);
+
+char *to_string(uint64_t num) {
+    uint64_t len = snprintf(NULL, 0, "%llu", num);
     char *str = malloc(len + 1);
     if (str == NULL) {
         return 0;
     }
-    snprintf(str, len + 1, "%d", num);
+    snprintf(str, len + 1, "%llu", num);
     return str;
 }
 
-int bin_to_dec(int bin) {
+uint64_t bin_to_dec(uint64_t bin) {
     char *str = to_string(bin);
     size_t length_str = strlen(str);
-    char *bintable = malloc(strlen(str) + 1);
-    int iter = 1;
-    if (bintable == NULL) {
-        return 0;
-    }
-    bintable[strlen(str)] = iter;
-    for (size_t count = length_str; count > 0; count--) {
-        int current = str[count];
-        // Return with -1 if input is not a binary number
-        if (current != 0 && current != 1) {
+    uint64_t *bintable = genbintable(str);
+    uint64_t result = 0;
+    for (size_t iter = 0; iter < length_str; iter++) {
+        if (str[iter] - '0' != 0 && str[iter] - '0' != 1) {
             return -1;
         }
+        result += (str[iter] - '0') * (bintable[iter]);
     }
-    return 1;
+    return result;
 }
 
-int *genbintable(char *binary) {
+uint64_t *genbintable(char *binary) {
     size_t strlen_binary = strlen(binary);
-    int *bintable = malloc(strlen_binary + 1);
+    uint64_t *bintable = malloc(strlen_binary + 1);
     if (bintable == NULL) {
         return 0;
     }
     for (size_t count = 0; count < strlen_binary; count++) {
         bintable[strlen_binary - count - 1] = (1 << count);
-        printf("fn bintable: %d\n", bintable[strlen_binary - count - 1]);
     }
-    printf("\n");
     return bintable;
 }
 
 int main() {
-    char binary[3] = {'a', 'b', 'c'};
-    int *bintable = genbintable(binary);
-    for (int i = 0; i < 3; i++) {
-        printf("bintable[%d]: %d\n", i, bintable[i]);
-    }
-    printf("%d\n", bin_to_dec(61));
+    printf("%llu\n", bin_to_dec(101010110101010010100110100101));
     return 1;
 };
